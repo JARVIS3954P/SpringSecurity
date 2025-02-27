276d96549499c9d0f3106988ebb4d2b175c74674
JARVIS3954P <gaintacolyte9@gmail.com>
2025-02-27 11:06:29 +0530

    Custom User Login

---
+++ spring-sec-demo/src/main/java/com/jarvis/spring_sec_demo/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.jarvis.spring_sec_demo.controller;
+
+import com.jarvis.spring_sec_demo.model.User;
+import com.jarvis.spring_sec_demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user){
+        System.out.println("Register endpoint hit with username: " + user.getUsername());
+        return service.saveUser(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user){
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
+
+        if(authentication.isAuthenticated())
+            return "Success";
+        else
+            return "Failed";
+    }
+}
---
+++ spring-sec-demo/src/main/java/com/jarvis/spring_sec_demo/model/User.java
@@ -0,0 +1,26 @@
+package com.jarvis.spring_sec_demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
